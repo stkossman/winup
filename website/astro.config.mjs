e2e2b233414392908edcf8499068b5cfacffc5a4
Andrii Stavskyi <an.stawski@outlook.com>
2026-05-16 21:46:31 +0300

    feat(website): add Prettier configuration and update styles for consistency

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://winup-cli.vercel.app/',
-	vite: {
-		plugins: [tailwindcss()],
-	},
-})
+    site: 'https://winup-cli.vercel.app/',
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
