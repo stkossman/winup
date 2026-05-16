c9e992cfbc3b5e31d61e8cba03ef7daded7059ff
Andrii Stavskyi <an.stawski@outlook.com>
2026-05-16 20:25:57 +0300

    feat(website): add Header component

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://winup.vercel.app',
 	vite: {
 		plugins: [tailwindcss()],
 	},
